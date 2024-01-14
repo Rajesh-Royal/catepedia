dcd2d5cf4ee600eb06ad7015a3f4b7c890e503cf
Rajesh-Royal <rajeshroyal896@gmail.com>
2024-01-14 10:14:47 +0530

    chore: catpedia logo component created

---
+++ components/Logo.vue
@@ -0,0 +1,8 @@
+<template>
+  <div
+    class="bg-white p-1 my-2 md:my-0 md:p-3 shadow-lg border border-dashed flex items-center"
+  >
+   <img src="../assets/images/cute-cat-head-element-png-logo.webp" class="w-[40px]"/>
+   <h2 class="font-bold text-3xl pl-2.5">Catepedia</h2>
+  </div>
+</template>
