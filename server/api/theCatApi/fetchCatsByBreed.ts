5259619104baba0868d8af760a7f1b1b13cb3004
Rajesh-Royal <rajeshroyal896@gmail.com>
2024-01-20 15:04:10 +0530

    api: use local database of cat breed with fuse.js

--- server/api/theCatApi/fetchCatsByBreed.ts
+++ server/api/theCatApi/fetchCatsByBreed.ts
@@ -1,6 +1,13 @@
+import catBreedData from '../../../assets/catBreeds.json';
+import Fuse from 'fuse.js'; 
+
+const fuse = new Fuse(catBreedData, {keys: ['name'], threshold: 0.3});
+
 export default defineEventHandler(async (event) => {
-  const { breed, limit = 9, page = 0 } = getQuery(event);
-  const catApiResponse: any = await $fetch(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`);
+  const { breed: breedQuery, limit = 9, page = 0 } = getQuery(event);
+  // const catApiResponse: any = await $fetch(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`);
+
+  const catApiResponse = fuse.search(breedQuery as string)?.map((result) => result.item);
 
   const startIndex = Number(page) * Number(limit);
   const results = catApiResponse.slice(
